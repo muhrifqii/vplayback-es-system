e38c15aee882c2e98065e27bbf62ff3d1a268fb6
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2025-05-29 01:26:24 +0700

    spring boot project initializr-ed

---
+++ vplayback/src/main/java/com/muhrifqii/vplayback/VplaybackApplication.java
@@ -0,0 +1,13 @@
+package com.muhrifqii.vplayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VplaybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VplaybackApplication.class, args);
+	}
+
+}
---
+++ vplayback/src/test/java/com/muhrifqii/vplayback/VplaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhrifqii.vplayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VplaybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
